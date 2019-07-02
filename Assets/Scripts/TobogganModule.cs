587659d4698a6d09153aeee85f778d7af5503038
Adrien Lucas <mrbrenan@live.fr>
2019-07-02 19:39:42 +0200

    [Add] Toboggan prefabs

---
+++ Assets/Scripts/TobogganModule.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Attach to : Toboggan prefab
+/// This component stores all key objects of the toboggan prefab
+/// </summary>
+public class TobogganModule : MonoBehaviour
+{
+    [SerializeField] private Transform path; //The gameObject storing all the path points
+    [SerializeField] private Transform startLink; //The point at the beginning of the toboggan
+}
