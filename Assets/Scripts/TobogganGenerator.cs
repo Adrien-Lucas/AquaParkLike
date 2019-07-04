6dbeb805055ab253cf8ae1d6d4360b7c9a25e5a7
Adrien Lucas <mrbrenan@live.fr>
2019-07-04 18:28:19 +0200

    [Add] Waypoint path

--- Assets/Scripts/TobogganGenerator.cs
+++ Assets/Scripts/TobogganGenerator.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TobogganGenerator : MonoBehaviour
 {
+    [Header("Toboggan parameters")]
+    
     [SerializeField] private Transform endPoint;
-
     [SerializeField] private List<Transform> types = new List<Transform>();
-
     [SerializeField] private Transform startModule;
-
     [SerializeField] private int length = 10;
 
+    [Header("Character spawner parameters")] 
+    
+    [SerializeField] private Transform playerPrefab;
+    
     private int _iterations = 0;
+    public static List<Vector3> TotalPath = new List<Vector3>();
 
     // Start is called before the first frame update
     void Start()
     {
+        TotalPath.Clear();
         GenerateParentModule(endPoint);
     }
 
@@ -31,6 +37,9 @@ public class TobogganGenerator : MonoBehaviour
         else
         {
             InstantiateModule(module, startModule);
+            //Path is cleaned from duplicates
+            TotalPath = TotalPath.Distinct().ToList();
+            SpawnCharacters(module.GetComponent<TobogganModule>());
         }
     }
 
@@ -43,6 +52,9 @@ public class TobogganGenerator : MonoBehaviour
         {
             Vector3 rotationAxis = Vector3.Cross(parent.forward, parent.up);
             parent.RotateAround(parent.position, rotationAxis, parent.GetComponent<TobogganModule>().angle);
+            
+            //Add module's path to total path
+            TotalPath.InsertRange(0, infos.Path);
         }
 
         if (infos.endLink)
@@ -52,5 +64,17 @@ public class TobogganGenerator : MonoBehaviour
         }
         
         return parent;
+    }
+
+    void SpawnCharacters(TobogganModule module)
+    {
+        SpawnPlayer(module);
+    }
+
+    void SpawnPlayer(TobogganModule module)
+    {
+        Instantiate(playerPrefab,
+            module.Path[0] + (module.Path[1] - module.Path[0]) * Random.Range(0f, 1f) + Vector3.up,
+            Quaternion.identity);
     }
 }
--- Assets/Scripts/TobogganModule.cs
+++ Assets/Scripts/TobogganModule.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 /// <summary>
@@ -8,7 +9,37 @@ using UnityEngine;
 /// </summary>
 public class TobogganModule : MonoBehaviour
 {
-    [SerializeField] private Transform path; //The gameObject storing all the path points
+    [SerializeField] private Transform pathHolder;
+
+    public List<Vector3> Path
+    {
+        get
+        {
+            if (pathHolder)
+            {
+                List<Vector3> path = pathHolder.GetComponentsInChildren<Transform>().Select(t => t.position).ToList();
+                path.Remove(pathHolder.position);
+                return path;
+            }
+                
+            return new List<Vector3>();
+        }
+    }
+
+    //The gameObject storing all the path points
     public Transform endLink; //The point at the end of the module
     public float angle;
+
+    [Header("Gizmos parameters")] [SerializeField]
+    private Color rayColor = Color.white;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = rayColor;
+        for (int i = 0; i < Path.Count; i++)
+        {
+            if(i > 0) Gizmos.DrawLine(Path[i - 1], Path[i]);
+            Gizmos.DrawWireSphere(Path[i], 0.5f);
+        }
+    }
 }
